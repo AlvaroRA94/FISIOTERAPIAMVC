6fb4cc85dc7eb871a6a2d9e2227833176cde3f0a
AlvaroR <roldan.antequera@gmail.com>
2024-06-10 22:00:11 +0200

    Modificación de Estilos

--- views/paginas/nosotros.php
+++ views/paginas/nosotros.php
@@ -17,17 +17,17 @@
 
             <p>Número de Colegiado: 29/10707</p>
 
-            <p>Titulado por la Universidad de Málaga. </p>
+            <p class="desbordamiento">Titulado por la Universidad de Málaga. </p>
 
-            <p>Máster en fisioterapia invasiva, formación oficial en dolor
-                crónico, <br> ejercicio terapéutico y control motor.</p>
+            <p class="desbordamiento">Máster en fisioterapia invasiva, formación oficial en dolor
+                crónico, ejercicio terapéutico y control motor.</p>
 
-            <p>Amplia experiencia tratando pacientes con dolor tanto agudo como crónico a través de
+            <p class="desbordamiento">Amplia experiencia tratando pacientes con dolor tanto agudo como crónico a través de
                 técnicas con la máxima evidencia científica.
             </p>
 
-            <p>He formado parte de diferentes equipos multidisciplinares que me <br> han permitido ampliar y
-                consolidar mis conocimientos además de <br> hacer uso de numerosas técnicas y tecnologías aplicadas
+            <p class="desbordamiento">He formado parte de diferentes equipos multidisciplinares que me han permitido ampliar y
+                consolidar mis conocimientos además de hacer uso de numerosas técnicas y tecnologías aplicadas
                 a la salud.
             </p>
 
--- views/paginas/servicio.php
+++ views/paginas/servicio.php
@@ -3,7 +3,7 @@
 
     <img loading="lazy" src="/imagenes/<?php echo $servicio->imagen; ?>" alt="imagen del servicio">
 
-    <div class="resumen-elemento">
+    <div class="resumen-elemento" style="padding:2.5rem;">
         <p class="precio"><?php echo $servicio->precio; ?> €</p>
         <p><?php echo $servicio->descripcion; ?></p>
     </div>
