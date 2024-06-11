b3a54db3c4988c24680945f58bfb5dd542a694c6
AlvaroR <roldan.antequera@gmail.com>
2024-06-11 17:41:53 +0200

    Modificacion BBDD Hosting

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,7 +1,11 @@
 <?php
 
 function conectarDB() : mysqli {
-    $db = new mysqli('localhost', 'root', '1234', 'fisioterapia_crud');
+    // CONEXIÓN LOCALHOST //
+    // $db = new mysqli('localhost', 'root', '1234', 'fisioterapia_crud');
+
+    // CONEXIÓN HOSTING //
+    $db = new mysqli('q19.h.filess.io:3307', 'fisioterapia_gasolineno', '481eabce32742d68071ba517af7bd7a1eca1f9c0', 'fisioterapia_gasolineno');
 
     if(!$db) {
         echo "Error, no se pudo realizar la conexión";
